1bd8c9692ed73c547147191b425413d85107b60e
Rafael Camargo <rac_arruda@uol.com.br>
2014-05-18 22:40:39 -0300

    adicionado validacao do formulario do usuario

--- src/main/webapp/resources/js/conteudo.js
+++ src/main/webapp/resources/js/conteudo.js
@@ -47,12 +47,33 @@ var salvarVotosUsuario = function(event){
 	event.preventDefault();
 	var nome = $("#formSalvaVotosUsuario").find('input[name="nome"]').val();
 	var email = $("#formSalvaVotosUsuario").find('input[name="email"]').val();
-	if(nome.length > 0 && email.length > 0){
+	
+	var mensagemDeInvalidacao = "Invalidações: ";
+	var nomeValido = false;
+	if(nome.length > 4){
+		nomeValido = true;
+	}else{
+		mensagemDeInvalidacao += "Email inválido!\n";
+	}
+	
+	var emailValido = false;
+	if( emailValido(email) ){
+		emailValido = true;
+	}else{
+		mensagemDeInvalidacao += "O nome deve ter no minimo 4 caracteres!\n";
+	}
+	
+	if(nomeValido && emailValido){
 		var uri = "usuario/salvar/livrosvotados?nome="+nome+"&email="+email;
 		$.get(uri, callback_salvarVotosUsuario);
 	}else{
-		alert("Preencha os campos nome e e-mail!");
+		alert(mensagemDeInvalidacao);
 	}
+};
+
+var emailValido = function(emailAddress){
+    var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
+    return pattern.test(emailAddress);
 };
 
 /**
